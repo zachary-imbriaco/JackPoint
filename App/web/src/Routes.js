1960877673f8952242a303af74311a85ecf80c23
Vaeliwen <zaman@imbriaco.com>
2020-12-17 13:52:37 -0500

    Fixed route.

--- App/web/src/Routes.js
+++ App/web/src/Routes.js
@@ -12,7 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/index" page={HomePage} name="home" />
+      <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
