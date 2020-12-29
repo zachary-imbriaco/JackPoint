53bbdac150a6d110e8582e707a321b431f859f6a
Vaeliwen <zaman@imbriaco.com>
2020-12-29 13:15:34 -0500

    Added primary layout to sheet page.

--- App/web/src/pages/SheetPage/SheetPage.js
+++ App/web/src/pages/SheetPage/SheetPage.js
@@ -1,4 +1,5 @@
 import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from '../../layouts/BlogLayout/BlogLayout'
 
 const SheetPage = () => {
   return <BlogLayout>Here's where you'll be sticking your runner's info, once it's up and running.</BlogLayout>
