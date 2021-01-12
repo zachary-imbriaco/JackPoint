14dc23283f70f964c1398525c94a8860c85f145b
Vaeliwen <zaman@imbriaco.com>
2021-01-12 12:05:10 -0500

    Parsing JSON and displaying test data.

--- App/web/src/pages/SheetPage/SheetPage.js
+++ App/web/src/pages/SheetPage/SheetPage.js
@@ -1,10 +1,20 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useState } from 'react'
 import BlogLayout from '../../layouts/BlogLayout/BlogLayout'
   
 const SheetPage = () => {
+  const [sheet, setSheet] = useState('')
+  const [parsed, setParsed] = useState({characters: {character: {metatype: 'Ork'}}})
+
+  const handleParse = () => {
+    setParsed(JSON.parse(sheet))
+    console.log(parsed)
+  }
+
   return <BlogLayout>
-    <input type='text' name='JSON'></input>
-    <button>submit</button>
+    <input type='text' placeholder='json' onChange={event => setSheet(event.target.value)}></input>
+    <button onClick={event => handleParse()}>Parse</button>
+    <p>Metatype: {parsed.characters.character.metatype}</p>
     </BlogLayout>
 }
 
