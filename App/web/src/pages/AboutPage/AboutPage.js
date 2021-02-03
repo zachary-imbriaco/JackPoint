2c686b52e5f954a167d947bb25120b0f678b613e
Vaeliwen <zaman@imbriaco.com>
2021-02-03 14:22:07 -0500

    Made a new about page.

--- App/web/src/pages/AboutPage/AboutPage.js
+++ App/web/src/pages/AboutPage/AboutPage.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
 
     <BlogLayout>
       <p>
-        Hoi, omae.  JackPoint is a character sheet repository for character sheets generated via the Chummer5a application.
+        Alright chummers, this site was designed to take in a Chummer export in JSON format, and give you a more aesthetically-pleasing character sheet from it.  It won't add stuff together for you like Chummer does, at least not yet, but it'll let you see what you've got, at least.
       </p>
       <Link to={routes.home()}>Return home</Link>
     </BlogLayout>
